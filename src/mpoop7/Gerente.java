85d482d36fd1af5306833064ec9e02ac622d90d1
X512J <X512J@LAPTOP-OPVL3P73>
2021-06-23 22:00:18 -0500

    Practica 7 primer commit

--- src/mpoop7/Gerente.java
+++ src/mpoop7/Gerente.java
@@ -7,7 +7,9 @@ package mpoop7;
 
 /**
  *
- * @author X512J
+ * @author Estefany Zavala
+ * @version 1.2 23.06.2021
+ *
  */
 public class Gerente extends Empleado{ //
     private float presupuesto;
